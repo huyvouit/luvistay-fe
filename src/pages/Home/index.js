c0770b46c53787c9d0773fc544c493c2661101aa
P <19521998@gm.uit.edu.vn>
2022-03-17 14:52:31 +0700

    merge and update folder

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 // components
 import HeroSlider from "../../components/HeroSlider";
-import BoxBooking from "../../components/home/boxbooking";
-import Banner from "../../components/Banner";
+import BoxBooking from "../../components/Home/BoxBooking";
+import Banner from "../../components/Home/Banner";
 
 const HomePage = () => {
   return (
