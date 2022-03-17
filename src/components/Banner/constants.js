54912761b1a4e04d8cbee0cbc5e0a70ac0a47d20
P <19521998@gm.uit.edu.vn>
2022-03-17 14:45:11 +0700

    P setup

---
+++ src/components/Banner/constants.js
@@ -0,0 +1,4 @@
+export const BANNER_IMAGE = {
+    BG_1: "https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/amenities-new.jpg",
+    THEME_1: "https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/image_testimonials.png",
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+// components
 import HeroSlider from "../../components/HeroSlider";
+import Banner from "../../components/Banner";
 
 const HomePage = () => {
-  return <HeroSlider />;
+  return (
+    <>
+      <HeroSlider />
+    </>
+  );
 };
 
 export default HomePage;
