2a1f403ed596846d13a70dd16dacf36868969aef
P <19521998@gm.uit.edu.vn>
2022-03-18 20:53:55 +0700

    outlined homepage

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -35,27 +35,27 @@ const BannerOne = ({}) => {
 
 const BannerTwo = ({}) => {
   return (
-    <div className="relative flex justify-center items-center h-[50vh] md:h-[80vh] overflow-hidden">
+    <div className="relative flex justify-center items-center h-[50vh] md:h-[80vh] overflow-hidden mb-12">
       <div className="__bg-banner_two bg-[url('https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/People-Say-.jpg')] bg-cover bg-no-repeat bg-center relative flex justify-center items-center w-full h-[85%] overflow-x-hidden"></div>
       <div className="__float-box-container_two absolute -inset-y-0 flex justify-center md:justify-start w-[100%] md:w-[90%]">
-        <div className="__float-box_two bg-[url('https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/image_testimonials.png')] bg-[#3f9cc1] bg-no-repeat bg-contain bg-left-top flex justify-center items-center h-full w-[90%] md:w-[50%] px-4 pt-10 pb-14 md:px-16 lg:px-20 md:py-20">
+        <div className="__float-box_two bg-[url('https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/image_testimonials.png')] bg-[#3f9cc1] bg-no-repeat bg-contain bg-left-top flex justify-center items-center h-full w-[90%] md:w-[50%] px-4 pt-5 sm:pt-10 pb-10 sm:pb-14 md:px-16 lg:px-20 md:py-20">
           <div className="flex flex-col space-y-10 justify-between h-full w-full text-white">
             <div className="flex flex-col justify-between h-[24%]">
-              <div className="h-1 w-1/5 bg-white"></div>
+              <div className="h-1 w-1/5 md:w-20 bg-white"></div>
               <h5 className="text-sm font-[500]">
                 AT THE HEART OF COMMUNITIES
               </h5>
               <h1 className="text-4xl md:text-2xl font-normal">People Say</h1>
             </div>
-            <div className="flex flex-col justify-between space-y-10 pl-10 h-[60%]">
+            <div className="flex flex-col justify-between space-y-10 pl-10 h-[70%] sm:h-[60%]">
               <p className="text-xl md:text-lg font-thin italic">
                 “We stayed here with our family and are fully satisfied with our
                 vacation. Rooms are very modern, have all needed amenities, the
                 kitchen is very delicious and service is just perfect. We will
                 for sure come back.”
               </p>
-              <div className="flex flex-row items-center w-full h-[55px]">
-                <div className="flex justify-center items-center h-full w-[55px] rounded-full bg-primary mr-4">
+              <div className="flex flex-row items-center w-full h-[42px] sm:h-[55px]">
+                <div className="flex justify-center items-center h-full w-[42px] sm:w-[55px] rounded-full bg-primary mr-4">
                   Avatar
                 </div>
                 <div className="flex flex-col space-y-0 text-base">
--- src/components/Home/Welcome/Welcome.jsx
+++ src/components/Home/Welcome/Welcome.jsx
@@ -3,8 +3,8 @@ import "./welcome.scss";
 
 const Welcome = () => {
   return (
-    <div className="flex flex-col space-y-14 sm:bg-red-400 sm:flex-row sm:h-[64vh] mx-5 mt-10 mb-10 sm:mb-20 pb-4">
-      <div className="__welcome-image-box bg-[url('https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/image_welcome.png')] bg-no-repeat bg-contain bg-center relative flex justify-start items-center h-[40vh]">
+    <div className="flex flex-col space-y-14 sm:space-y-10 sm:flex-row sm:h-[64vh] mx-5 mt-10 mb-10 sm:mb-20 pb-4">
+      <div className="__welcome-image-box bg-[url('https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/image_welcome.png')] bg-no-repeat bg-contain bg-center relative flex justify-start items-center h-[40vh] sm:h-[50vh] sm:w-full">
         <img
           className="absolute left-0 bottom-0 z-10 w-[50%] h-[76%]"
           src="https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/welcome-1-902x1024.jpg"
@@ -14,20 +14,20 @@ const Welcome = () => {
           src="https://themes.getmotopress.com/luviana/wp-content/uploads/sites/27/2019/07/welcome-2-1024x705.jpg"
         />
       </div>
-        <div className="__welcome-content-box h-[60vh] flex flex-col justify-start pl-5">
-        <div className="flex flex-col justify-evenly h-[25%] pt-4 pb-2">
-          <div className="w-[18%] bg-primary h-1" />
-          <div className="text-sm font-[450]">
+        <div className="__welcome-content-box h-[60vh] sm:w-[100%] flex flex-col justify-start pl-5">
+        <div className="flex flex-col justify-evenly sm:space-y-4 h-[30%] pt-4 sm:pt-0 pb-2">
+          <div className="w-20 bg-primary h-1" />
+          <div className="text-sm font-[450] sm:font-[500] tracking-wide">
             RAISING COMFORT TO THE HIGHEST LEVEL
           </div>
-          <div className="text-3xl md:text-2xl font-[500]">
+          <div className="text-3xl sm:text-5xl font-[500] tracking-wide">
             Welcome to LuviStay Hotel Resort
           </div>
         </div>
-        <div className="flex flex-col justify-evenly h-[75%] pl-12">
+        <div className="flex flex-col justify-evenly h-[70%] pl-12">
             <p className="text-gray-500 text-lg leading-7">The Hotel Luviana is the right choice for visitors who are searching for a combination of charm and a convenient position from where to explore surroundings.</p>
             <p className="text-gray-500 text-lg leading-7">The rooms are arranged on the first, second and third floors. On the top floor, there is also a charming terrace or solarium available for the use of guests, from where you can enjoy the view.</p>
-            <div className="flex justify-center items-center w-44 h-14 text-sm text-white font-mono rounded-full bg-primary"><a>READ MORE</a></div>
+            <div className="flex justify-center items-center w-44 h-14 text-sm text-white font-mono rounded-full bg-primary cursor-pointer"><a href="#" className="text-white">READ MORE</a></div>
         </div>
       </div>
     </div>
