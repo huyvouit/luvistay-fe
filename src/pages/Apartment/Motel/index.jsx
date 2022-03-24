eaa9fe38429bacb6301a5b7a4930bc409eb1627c
huyvouit <huyvouit@gmail.com>
2022-03-24 21:38:44 +0700

    add error page

--- src/pages/Apartment/Motel/index.jsx
+++ src/pages/Apartment/Motel/index.jsx
@@ -7,8 +7,8 @@ const MotelPage = () => {
   return (
     <main>
       <PageHeader title="Rooms & Suites" />
-      <PageTitle title="The hotel is arranged on three floors, without a lift. On the ground floor, apart from the reception, there is a comfortable lounge where you can sit and drink tea."/>
-      <Motel/>
+      <PageTitle title="The hotel is arranged on three floors, without a lift. On the ground floor, apart from the reception, there is a comfortable lounge where you can sit and drink tea." />
+      <Motel />
     </main>
   );
 };
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,4 +1,5 @@
 export const APP_ROUTE = {
+  ERROR: "/*",
   HOME: "/home",
   SIGNIN: "/signin",
   SIGNUP: "/signup",
