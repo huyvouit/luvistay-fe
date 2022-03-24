c1c00a8649e07869d86469847b1fa690d8806f26
NguyenTraVi <19522512@gm.uit.edu.vn>
2022-03-24 14:41:40 +0700

    hotel

--- src/pages/Apartment/Hotel/index.jsx
+++ src/pages/Apartment/Hotel/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hotel from "../../../components/Apartment/Hotel/Hotel";
+import PageHeader from "../../../components/PageHeader";
+import PageTitle from "../../../components/PageTitle";
 
 const HotelPage = () => {
-  return <div>HotelPage</div>;
+  return (
+    <main>
+      <PageHeader title="Rooms & Suites" />
+      <PageTitle title="The hotel is arranged on three floors, without a lift. On the ground floor, apart from the reception, there is a comfortable lounge where you can sit and drink tea."/>
+      <Hotel/>
+    </main>
+  );
 };
 
 export default HotelPage;
