e437c8b181dbc45cf674519979c72df8eaea1e76
huyvouit <huyvouit@gmail.com>
2022-03-27 01:24:57 +0700

    update detail

--- src/components/DetailApartment/Reviews/index.jsx
+++ src/components/DetailApartment/Reviews/index.jsx
@@ -15,19 +15,28 @@ const ReviewsDetail = () => {
         <PrimaryButton title="write a review" />
       </section>
       <section className="reviews-comments">
-        <img
-          src={`https://secure.gravatar.com/avatar/e913c28282474af166ee791c0f7fae7c?s=64&r=g`}
-          alt=""
-        />
-        <section className="reviews-comments-content">
-          <p className="reviews-name">NATHAN REYNOLDS</p>
-          <p className="reviews-date">JULY 16, 2019 AT 2:04 PM</p>
-          <StarRating ratingVote="5" />
-          <p className="reviews-desc">
-            Room is fully equipped and design is very modern and soothing. View
-            from the window is breathtaking.
-          </p>
-        </section>
+        {[...Array(2)].map((star, index) => {
+          return (
+            <section className="reviews-comments-content">
+              <img
+                src={`https://secure.gravatar.com/avatar/e913c28282474af166ee791c0f7fae7c?s=64&r=g`}
+                alt=""
+                className="reviews-avatar"
+              />
+              <section className="reviews-comments-body">
+                <p className="reviews-name">NATHAN REYNOLDS</p>
+                <p className="reviews-date">JULY 16, 2019 AT 2:04 PM</p>
+                <section className="reviews-rating">
+                  <StarRating ratingVote="5" />
+                </section>
+                <p className="reviews-desc">
+                  Room is fully equipped and design is very modern and soothing.
+                  View from the window is breathtaking.
+                </p>
+              </section>
+            </section>
+          );
+        })}
       </section>
     </main>
   );
