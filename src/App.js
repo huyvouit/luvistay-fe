17dbbc8102f8d13488860e6a3c9e8fbf5dffa277
P <19521998@gm.uit.edu.vn>
2022-05-11 08:41:22 +0700

    Phat/feat: Redux setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.scss";
 import "./main.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {fas} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import AppRoutes from "./routes/routes";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 library.add(fas);
 
 function App() {
-  return <AppRoutes />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/redux/Slice/apartmentSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {} from "../store";
+
+const initialState = {};
+
+const apartmentSlice = createSlice({
+  name: "apartmentSlice",
+  initialState,
+  reducers: {},
+});
+
+export const {} = apartmentSlice.actions;
+
+export default apartmentSlice.reducer;
