f55dc1cefa8b612665d19eb902ed0c3ac543a7e6
huyvouit <huyvouit@gmail.com>
2022-05-17 10:36:07 +0700

    update booking confirm

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
-import { MenuItem, Select, TextField } from "@mui/material";
-
+import {
+  MenuItem,
+  Select,
+  TextField,
+  TextareaAutosize,
+  Stack,
+  Typography,
+} from "@mui/material";
+import Radio from "@mui/material/Radio";
+// import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+// import FormControl from "@mui/material/FormControl";
+// import FormLabel from "@mui/material/FormLabel";
 import PageHeader from "../../components/PageHeader";
-
+import PrimaryButton from "../../components/PrimaryButton";
 import "./checkout.scss";
 const CheckoutPage = () => {
   const [openPrice, setOpenPrice] = useState(false);
+  const [selectedValue, setSelectedValue] = React.useState("a");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
   return (
     <main className="checkout-page">
       <PageHeader title="Booking Confirmation" />
@@ -27,28 +43,6 @@ const CheckoutPage = () => {
               </span>
             </p>
           </section>
-          <section className="checkout-accommodation">
-            <p className="checkout-title">Accommodation #1</p>
-            <p className="checkout-key">
-              Accommodation Type : <span>Standard Single Room</span>
-            </p>
-            <section className="inputField">
-              <p className="inputField-title">
-                Adults:<span>*</span>{" "}
-              </p>
-              <Select defaultValue="1" className="selectField">
-                {[...Array(2)].map((x, i) => (
-                  <MenuItem value={i + 1} key={i}>
-                    {i + 1}
-                  </MenuItem>
-                ))}
-              </Select>
-            </section>
-            <section className="inputField">
-              <p className="inputField-title">Full Guest Name</p>
-              <TextField variant="outlined" className="textField" />
-            </section>
-          </section>
         </section>
         <section className="checkout-content">
           <section className="checkout-content-price">
@@ -57,7 +51,7 @@ const CheckoutPage = () => {
               <colgroup
                 style={{ width: "500px", backgroundColor: "white" }}
               ></colgroup>
-              <colgroup style={{ backgroundColor: "#fcfcfc" }}></colgroup>
+              <colgroup></colgroup>
               <tr>
                 <th onClick={() => setOpenPrice(!openPrice)}>
                   #1 Standard Single Room
@@ -65,36 +59,126 @@ const CheckoutPage = () => {
                 <td>$129</td>
               </tr>
               {openPrice && (
-                <tr>
-                  <th>đ</th>
-                  <td>đ</td>
-                </tr>
+                <>
+                  <tr>
+                    <th>People</th>
+                    <td>7</td>
+                  </tr>
+                  <tr>
+                    <th>Nights</th>
+                    <td>2</td>
+                  </tr>
+                </>
               )}
+              <tr style={{ backgroundColor: "#e4e4e4" }}>
+                <th>Total</th>
+                <td>129000 VND</td>
+              </tr>
+            </table>
+            <table>
+              <colgroup
+                style={{ width: "500px", backgroundColor: "white" }}
+              ></colgroup>
+              <colgroup></colgroup>
+              <tr>
+                <th onClick={() => setOpenPrice(!openPrice)}>
+                  #1 Standard Single Room
+                </th>
+                <td>$129</td>
+              </tr>
+              {openPrice && (
+                <>
+                  <tr>
+                    <th>People</th>
+                    <td>7</td>
+                  </tr>
+                  <tr>
+                    <th>Nights</th>
+                    <td>2</td>
+                  </tr>
+                </>
+              )}
+              <tr style={{ backgroundColor: "#e4e4e4" }}>
+                <th>Total</th>
+                <td>129000 VND</td>
+              </tr>
             </table>
           </section>
-          <section className="checkout-accommodation">
-            <p className="checkout-title">Accommodation #1</p>
-            <p className="checkout-key">
-              Accommodation Type : <span>Standard Single Room</span>
+        </section>
+        <section className="checkout-content">
+          <section className="checkout-information">
+            <p className="checkout-title">Your Information</p>
+            <p className="checkout-required">
+              {`Required fields are followed by: *`}
             </p>
             <section className="inputField">
               <p className="inputField-title">
-                Adults:<span>*</span>{" "}
-              </p>
-              <Select defaultValue="1" className="selectField">
-                {[...Array(2)].map((x, i) => (
-                  <MenuItem value={i + 1} key={i}>
-                    {i + 1}
-                  </MenuItem>
-                ))}
-              </Select>
-            </section>
-            <section className="inputField">
-              <p className="inputField-title">
-                Check-in:<span>*</span>{" "}
+                Full Name:<span>*</span>{" "}
               </p>
               <TextField variant="outlined" className="textField" />
             </section>
+            <section className="inputField">
+              <p className="inputField-title">
+                Email:<span>*</span>{" "}
+              </p>
+              <TextField variant="outlined" className="textField" />
+            </section>
+            <section className="inputField">
+              <p className="inputField-title">
+                Number Phone:<span>*</span>{" "}
+              </p>
+              <TextField variant="outlined" className="textField" />
+            </section>
+            <section className="inputField">
+              <p className="inputField-title">
+                Personal ID:<span>*</span>{" "}
+              </p>
+              <TextField variant="outlined" className="textField" />
+            </section>
+            <section className="inputField">
+              <p className="inputField-title">Note:</p>
+              <TextareaAutosize className="textField" minRows={3} />
+            </section>
+          </section>
+        </section>
+        <section className="checkout-content">
+          <section className="checkout-payment">
+            <p className="checkout-title">Payment</p>
+            <section className="payment-selection">
+              <section className="payment-item">
+                <Radio
+                  checked={selectedValue === "a"}
+                  onChange={handleChange}
+                  value="a"
+                  name="radio-buttons"
+                  inputProps={{ "aria-label": "A" }}
+                />
+                <Typography className="payment-label">
+                  Pay on Arrival
+                </Typography>
+              </section>
+              <Typography>Pay with cash on arrival.</Typography>
+            </section>
+            <section className="payment-selection">
+              <section className="payment-item">
+                <Radio
+                  checked={selectedValue === "b"}
+                  onChange={handleChange}
+                  value="b"
+                  name="radio-buttons"
+                  inputProps={{ "aria-label": "B" }}
+                />
+                <Typography className="payment-label">Pay via Momo</Typography>
+              </section>
+              <Typography>
+                Visa, MasterCard, Discover, or American Express. Use the card
+                number 5555555555554444 with CVC 123 and a valid expiration date
+                to test a payment.
+              </Typography>
+            </section>
+          </section>
+          <section style={{ width: "200px", marginTop: 30 }}>
+            <PrimaryButton title="Book Now" />
           </section>
         </section>
       </section>
