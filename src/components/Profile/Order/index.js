e8ede33d6043b20cd7ce009ed7c288d28ece0c08
NguyenTraVi <19522512@gm.uit.edu.vn>
2022-05-18 15:11:58 +0700

    finish ui profile

--- src/components/Profile/Order/index.js
+++ src/components/Profile/Order/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SidebarProfile from '../Sidebar'
+import { Link } from "react-router-dom";
 import "./order.scss"
 
 const Order = () => {
@@ -13,17 +14,30 @@ const Order = () => {
                     <div className='order-container-row-two-container'>
                         <div className='colum-one'>
                             <div className='colum-one-container'>
-                                <SidebarProfile/>
+                                <SidebarProfile />
                                 <h2 className='colum-one-container-logout'>LOGOUT</h2>
                             </div>
                         </div>
                         <div className='colum-two'>
-                            <div className='colum-two-container-one'>
-                                <h1>Orders tracking</h1>
-                                <p>No order found. Shopping now!</p>
-                            </div>
-                            <div className='colum-two-container-two'>
-                                
+                            <div className='colum-two-container'>
+                                <h1 className='colum-two-container-title'>Orders tracking</h1>
+                                <p className='colum-two-container-description'>No order found. Shopping now!</p>
+                                <div className='table-order'>
+                                    <div className='table-order-row'>
+                                        <p className='table-order-colum-one table-title'>ID</p>
+                                        <p className='table-order-colum-one table-title'>Date</p>
+                                        <p className='table-order-colum-two table-title'>Hotel</p>
+                                        <p className='table-order-colum-two table-title'>Total</p>
+                                    </div>
+                                    <Link to="/profile" className='table-container'>
+                                        <div className='table-order-row'>
+                                            <p className='table-order-colum-one'>#1306552651656513516845</p>
+                                            <p className='table-order-colum-one'>30/05/2022 - 05/06/2022</p>
+                                            <p className='table-order-colum-two'>LuviStay</p>
+                                            <p className='table-order-colum-two'>5.000.000 VNĐ</p>
+                                        </div>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
