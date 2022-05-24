60233cbdb389d975d8ecbae4e8943a0ee44218e3
NguyenTraVi <19522512@gm.uit.edu.vn>
2022-05-24 17:24:44 +0700

    boxleft of  blog

--- src/components/Blog/BoxLeft/index.js
+++ src/components/Blog/BoxLeft/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./boxLeft.scss";
 
 const BoxLeft = () => {
+  const management = [
+    { managementId: "/blog", name: "ACCOUNT DETAIL" },
+    { managementId: "/my-blog", name: "CHANGE PASSWORD" },
+  ];
+
+  const [locationHref, setLocationHref] = useState("");
+  useEffect(() => {
+    let wlh = window.location.href;
+    for (let i = 0; i < 2; i++) {
+      if (wlh.includes(management[i].managementId))
+        setLocationHref( management[i].managementId);
+    }
+  }, [window.location.href]);
   return (
-    <div>BoxLeft</div>
-  )
+    <>
+      {management.map((item, index) => {
+        let path = item.managementId;
+        return (
+              <Link
+                to={path}
+                className={
+                  locationHref === path
+                    ? "blog-aSelect"
+                    : "blog-a"
+                }
+              >
+                <h2>{item.name}</h2>
+              </Link>
+        );
+      })}
+    </>
+  );
 }
 
 export default BoxLeft
