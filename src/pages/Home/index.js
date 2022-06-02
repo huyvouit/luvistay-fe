db84bb2e0056eb3f567840c3773ea9b454e03e2a
huyvouit <huyvouit@gmail.com>
2022-06-02 23:12:41 +0700

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -28,11 +28,14 @@ const HomePage = () => {
       <ReviewTwo />
       <div className="__maps_embed">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.2311711967195!2d106.80086005103158!3d10.87001416039203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2sUniversity%20of%20Information%20Technology%20-%20VNUHCM!5e0!3m2!1sen!2s!4v1648476395213!5m2!1sen!2s"
-          style={{ border: "0", width: "100%", height: "500px" }}
-          allowFullScreen={true}
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.123450308989!2d106.80327138400044!3d10.868816298372641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175273ccf83c687%3A0x9c14bbc57c56a121!2sLuvistay!5e0!3m2!1svi!2s!4v1654186242144!5m2!1svi!2s"
+          width="600"
+          height="450"
+          style={{ border: 0, width: "100%" }}
+          allowfullscreen=""
           loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
+          referrerpolicy="no-referrer-when-downgrade"
+          title="luviStay"
         ></iframe>
       </div>
     </>
